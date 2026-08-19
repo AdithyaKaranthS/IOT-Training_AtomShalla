7ceaa5a3a53e30c338e563dd8d421f348bcea33c
Aditya Karanth S <adxsuper@gmail.com>
2026-08-19 10:07:42 +0530

    Updated project

---
+++ Days_switch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Days_switch {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a day number (1-7): ");
+		int day = scanner.nextInt();
+
+		switch (day) {
+			case 1:
+				System.out.println("It is Sunday.");
+				break;
+			case 2:
+				System.out.println("It is Monday.");
+				break;
+			case 3:
+				System.out.println("It is Tuesday.");
+				break;
+			case 4:
+				System.out.println("It is Wednesday.");
+				break;
+			case 5:
+				System.out.println("It is Thursday.");
+				break;
+			case 6:
+				System.out.println("It is Friday.");
+				break;
+			case 7:
+				System.out.println("It is Saturday.");
+				break;
+			default:
+				System.out.println("Invalid day number.");
+		}
+
+		scanner.close();
+	}
+}
