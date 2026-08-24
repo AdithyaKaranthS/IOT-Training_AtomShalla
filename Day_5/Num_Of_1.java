ff685cdddff94b41b3ef58178fb91f96e58a94b4
Aditya Karanth S <adxsuper@gmail.com>
2026-08-24 09:26:57 +0530

    Updated Project

--- Day_5/Num_Of_1.java
+++ Day_5/Num_Of_1.java
@@ -7,6 +7,12 @@ class Num_Of_1 {
 
         System.out.println("Enter 6 numbers:");
         for (int i = 1; i <= 6; i++) {
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please enter 6 valid numbers.");
+                scanner.close();
+                return;
+            }
+
             int num = scanner.nextInt();
             if (num == 1) {
                 count++;
