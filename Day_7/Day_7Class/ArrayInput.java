98bd3147b87c592316bef4198400706ceb1df4d1
Aditya Karanth S <adxsuper@gmail.com>
2026-08-27 15:31:27 +0530

    Updated Project

---
+++ Day_7/Day_7Class/ArrayInput.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArrayInput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        System.out.println("Enter " + (n * n) + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("2D array elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
